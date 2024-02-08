4ba350e61c71de64187e95279ea536a244cecc3c
365daysofc0ding <159339635+365daysofc0ding@users.noreply.github.com>
2024-02-08 20:19:47 +0100

    A simple age calculator with error handling

--- src/main/kotlin/AgeCalculator.kt
+++ src/main/kotlin/AgeCalculator.kt
@@ -1,3 +1,28 @@
+import java.time.LocalDate
+
+val currentDate = LocalDate.now()
+val currentYear = currentDate.year
+var birthYear = 0
+
+fun calcAge(birthYear: Int): Int {
+    return currentYear - birthYear
+}
+
 fun main() {
-    println("Hello World")
+
+    do {
+        println("What year were you born?")
+        try {
+            val userInput = readln().toInt()
+            if (userInput >= 1900) {
+                birthYear = userInput
+            } else {
+                println("Birth year must be at least 1900 \n")
+            }
+        } catch (exception: NumberFormatException) {
+            println("Number not an integer \n")
+        }
+    } while (birthYear == 0)
+
+    println("You turn ${calcAge(birthYear)} this year 🎉")
 }
